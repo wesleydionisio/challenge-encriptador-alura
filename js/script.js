cf29580602ff0d5a911d1ab4c4eab6f78cef697c
Wesley Dionisio <wesley.r.dionisio@gmail.com>
2022-08-23 14:30:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+const copy = document.querySelector("#copy");
+// document.getElementById("div-aparece").style.display = 'none';
+
+
+//Encriptar mensagem
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImage = "none"
+    aparece()
+}
+
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(inputTexto.value)
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImage = "none"
+    aparece()
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+
+copy.addEventListener("click", () => {
+    let copiado = mensagem.value;
+
+    navigator.clipboard.writeText(copiado).then(() => {
+        copy.textContent = "Copiado";
+        copy.classList.add("copiedButton");
+
+        window.setTimeout(() => {
+            copy.textContent = "Copiar";
+            copy.classList.remove("copiedButton");
+        }, 1000);
+    });
+});
+
+
+
